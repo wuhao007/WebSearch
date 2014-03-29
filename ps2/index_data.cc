29257b2a53b2bc69ddb074aca8db008620b4563d
Hao Wu <tohaowu@gmail.com>
2014-03-29 21:55:08 +0000

    fix bug of page size

--- ps2/index_data.cc
+++ ps2/index_data.cc
@@ -43,60 +43,83 @@ string memAlloc(gzFile* fileName, int size)
 
 int main (int argc, char* argv[])
 {
+    const string docID_url = "docID_url.rpt";
     long doc_num = atoi(argv[2]);
+    //cout << "doc_num " << doc_num << endl;
+
     gzFile *cData,*cIndex;
     ofstream myfile;
+
     int j = atoi(argv[1]);
-    cout << j << endl;
+    //cout << "j " << j << endl;
+
     ostringstream start;
     ostringstream end;
+
     start << j;
+
     int n = (j == 4100)? 79:99;
+
     end << (j + 99);
+
     string folder_name("vol_" + start.str() + "_" + end.str() + "/");
+
     for (int i = 0; i <= n; i++)
     {
         ostringstream convert;
         convert << (i+j);
+
         string file_num(folder_name + convert.str());
-        cout << file_num << endl;
+        //cout << file_num << endl;
+
         cIndex=static_cast<void**>(gzopen((file_num + "_index").c_str(),"r"));
         cData=static_cast<void**>(gzopen((file_num + "_data").c_str(),"r"));
+
         vector<string> indexes = split(memAlloc(cIndex, INDEX_CHUNK), '\n');
-        int length = 0;
-        string docID_url = "docID_url.rpt";
+
         for (vector<string>::iterator it = indexes.begin(); it != indexes.end(); ++it)
         {
             vector<string> status = split(*it);
+            cout << *it << endl;
             string url = status[0];
             string len = status[3];
-            length += atoi(len.c_str());
+
+            //cout << "size " << atoi(len.c_str()) << " length " << length << endl;
+            int length = atoi(len.c_str());
+            //cout << "length " << length << endl;
             char* buffer=(char*)malloc(length);
             gzread(cData,buffer,length);
             char* pool = (char*)malloc(2*length+1);
             int ret = parser(buffer, pool, 2*length+1);
+            //cout << buffer << endl;
 
             myfile.open(docID_url.c_str(), ios::app | ios::binary);
-            myfile << doc_num++ << " " << url << endl;
+            myfile << doc_num << " " << url << endl;
+            cout << "doc_num " << doc_num << endl;
             myfile.close();
 
             myfile.open((file_num + ".txt").c_str(), ios::app | ios::binary);
+            cout << "ret " << ret << endl;
 
             if (ret > 0)
             {
+                //cout << "parse_num " << doc_num << endl;
                 vector<string> words = split(pool, '\n');
-                for (vector<string>::iterator it = words.begin(); it != words.end(); it++)
+                for (vector<string>::iterator wit = words.begin(); wit != words.end(); wit++)
                 {
-                    vector<string> word = split(*it);
-                    myfile << word[0] << " " << doc_num << endl;
+                    ostringstream num_convert;
+                    num_convert << doc_num;
+                    string docNum(num_convert.str());
+                    vector<string> word = split(*wit);
+                    myfile << word[0] << ' ' << string(7-docNum.size(),' ') << docNum << endl;
                 }
             }
             myfile.close();
 
             free(pool);
             free(buffer);
+            doc_num++;
         }
     }
     return 0;
 }
-
--- ps2/sort.sh
+++ ps2/sort.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-rm -f *txt *rpt *map vol_*/*txt 
-g++ -w index_data.cc split.cc split.h parser.h parser.c -lz
-mv a.out index_data
-g++ -w merge.cc split.cc split.h
-mv a.out merge
+rm -rf data *txt *rpt *map vol_*
+./make
+#g++ -w index_data.cc split.cc split.h parser.h parser.c -lz
+#mv a.out index_data
+#g++ -w merge.cc split.cc split.h
+#mv a.out merge
 read -p "Please input start number: " start
 read -p "Please input stop number: " stop
 for (( i=${start}; i<${stop}; i=i+100 ))
@@ -12,12 +13,13 @@ do
     mv data/4c/tux-4/polybot/gzipped_sorted_nz/* .
     line=`tail -n 1 docID_url.rpt | awk '{print $1}'`
     line=$((${line}+1))
-    echo $line
+    echo "line:"$line
+    #echo $line
     ./index_data $i $line
     dir=vol_${i}_*
-    echo $dir
+    #echo $dir
     #sort -u $dir/*txt > sort_${i}.txt
     sort $dir/*txt > sort_${i}.txt
 done
 sort -m sort_*.txt > merge.rpt
-./merge
+#python merge.py
