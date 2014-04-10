c0644e925ea97765e4c7b8a1a1f845df501f07cc
Hao Wu <tohaowu@gmail.com>
2014-04-10 16:01:02 -0400

    sync code

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@
 ###
 
 import json
+from webcorpus import WebCorpus
 from getpage import get_page
 
 def get_all_friends(page):
@@ -17,11 +18,13 @@ def get_all_friends(page):
 def crawl_web(seed): # returns index, graph of inlinks
     tocrawl = set([seed])
     crawled = []
+    corpus = WebCorpus()
     while tocrawl: 
         id = tocrawl.pop() # changed page to url - clearer name
         if id not in crawled:
             content = get_page(id)
             friends = get_all_friends(content)
+            corpus.add_friend(id, friends)
             tocrawl.update(friends)
             crawled.append(id)
     return crawled
--- getpage.py
+++ getpage.py
@@ -2,7 +2,6 @@ def real_get_page(id):
     import urllib
     print id
     url = 'https://api.foursquare.com/v2/users/' + id + '?v=040914&oauth_token=MGRUDACH5ZCGPSEA5PJIOH05UKOUT1WYMRNDIHPVX5GV0TMA'
-    print url
     try:
         return urllib.urlopen(url).read()
     except:
