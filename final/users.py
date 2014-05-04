a75a92ce3a9a6d07944c2b3c88d68aa5b9759a22
Hongzheng Shi <JeremyShi@172-29-32-221.dynapool.nyu.edu>
2014-05-04 11:39:26 -0400

    rm sss

--- final/users.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'JeremyShi'
-
-import config
-
-def writeUser(user):
-    try:
-        fout=open(config.userFile,'a')
-        line = '%s '%(user.id,user.firstName,user.lastName,user.gender,user.homeCity,
-        fout.write()
